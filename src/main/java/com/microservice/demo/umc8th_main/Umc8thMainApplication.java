892c681f4fd91bd96fddbfe0a60ede69c5d91e3e
Jimyeong Kim <kjm021221@icloud.com>
2025-04-04 12:15:35 +0900

    first commit

---
+++ src/main/java/com/microservice/demo/umc8th_main/Umc8thMainApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.umc8th_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc8thMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Umc8thMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/demo/umc8th_main/Umc8thMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.umc8th_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc8thMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
