eaa683cafd89225ff055b32db705fa8102c07649
Jimyeong Kim <kjm021221@icloud.com>
2025-05-05 13:20:36 +0900

    ✨feat: Temp 테스트 패키지 작성

---
+++ src/main/java/com/microservice/demo/umc8th_main/domain/temp/converter/TempConverter.java
@@ -0,0 +1,18 @@
+package com.microservice.demo.umc8th_main.domain.temp.converter;
+
+import com.microservice.demo.umc8th_main.domain.temp.dto.TempResponse;
+
+public class TempConverter {
+
+    public static TempResponse.TempTestDTO toTempTestDTO(){
+        return TempResponse.TempTestDTO.builder()
+                .testString("This is Test!")
+                .build();
+    }
+
+    public static TempResponse.TempExceptionDTO toTempExceptionDTO(Integer flag){
+        return TempResponse.TempExceptionDTO.builder()
+                .flag(flag)
+                .build();
+    }
+}
